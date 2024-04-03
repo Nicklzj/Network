c009bb0398ccb207fed09b123ac27d125491de05
Nicklzj11 <1544252715@qq.com>
2024-04-03 16:26:28 +0800

    delete many2

--- fast.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "# Network" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Nicklzj/Network.git
-git push -u origin main
